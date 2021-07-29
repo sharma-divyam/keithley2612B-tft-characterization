364fefcc6fa57aab5c72fda323c0aa59f58e4112
sharma-divyam <divyam16september1999@gmail.com>
2021-07-29 22:37:13 +0530

    Initialize repository

---
+++ Kiethley2600.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  Keithley 2612B SMU TFT Characterization
+
+#  Libraries
+
+from keithley2600 import Keithley2600
+import numpy as np
+import os
+import pyvisa
+
+
+# Connecting the instrument
+
+rm = pyvisa.ResourceManager()
+rm.list_resources()
+address = []
+
+if len(rm.list_resources()) == 0:
+    print ('No device connected')
+    
+else:  
+    for item in rm.list_resources():
+        address.extend(item)
+        
+    print ('VISA address of the connected devices are:')
+    
+    for i in range(len(address)):
+        print (f"i: {address[i]}")
+    
+    
+    is_valid_input = False 
+    
+    while(not is_valid_input):
+        
+        connection_check = input ('Is the VISA address of the SMU listed? (Y/N)')
+        
+        if connection_check == 'Y' or  connection_check == 'y':
+            is_valid_input = True
+            smu_index = input('Enter the index number of the SMU:')
+            k = Keithley2600(address[smu_index])
+            print ('Connected successfully!')
+
+        
+        elif connection_check == 'N' or connection_check == 'n': 
+            is_valid_input = True 
+            print ('Please ensure that the SMU is connected properly')
+    
+        else: 
+            print ('Invalid entry. Please try again.')
+        
+   
+
+
+
+
+
+
+
+
