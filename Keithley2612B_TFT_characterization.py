f793b155c09850ed4a0675afb6282a06a95f037f
sharma-divyam <divyam16september1999@gmail.com>
2021-08-03 02:39:19 +0530

    Reduce while loops by introducing functions

--- Keithley2612B_TFT_characterization.py
+++ Keithley2612B_TFT_characterization.py
@@ -11,108 +11,124 @@ import os
 import pyvisa
 
 
+def get_target_volt(start_volt):
+    """
+    Return the target volt. Check validity of the input.
+
+    :param start_volt: The starting point of the voltage sweep. 
+
+    """
+    while True:
+        # Keeps asking the user for the valid target volt until they correclty provide one.
+        target_volt = float (input ('Set the target voltage (in Volts) for sweep:'))
+        
+        if target_volt > start_volt:
+            return target_volt
+        else: 
+            print ('INVALID INPUT: Target voltage must be greater than starting voltage.')
+
+def get_step_volt(start_volt, target_volt):
+    """
+    Return the step volt. It is the magnitude of steps in which the sweep will occur. Check validity of the input.
+
+    :param start_volt: The starting point of the voltage sweep. 
+    :param target_volt: The end point of the voltage sweep. 
+
+    """
+    while True:
+
+        step_volt = float (input ('Set the size of steps (in Volts) of sweep:')) 
+        
+        if step_volt <= target_volt - start_volt:
+            return step_volt
+        else:
+            print ('INVALID INPUT: Step must be less than or equal to the difference between starting and target voltage')
+
+def get_integration_time ():
+    """
+    Return integration time. Check if input is within the range suggested by Keithley2600 manual.
+
+    """
+    while True:
+        integration_time = float (input('Set the integration time for each data point (in PLC)'))
+        if integration_time >= 0.001 and integration_time <= 25:
+            return integration_time
+        else: 
+            print('INVALID INPUT: The integration time must be between 0.001 and 25 PLC.')
+
+def get_sweep_type():
+    """
+    Return boolean for sweep type based on the character input.
+    """
+    while True:
+        sweep_type = input ('Choose between pulsed and continuous sweep (enter P or C):')
+        if sweep_type == 'P' or sweep_type == 'p':
+            return True  
+        elif sweep_type == 'C' or sweep_type == 'c':
+            return False
+        else:
+            print ('INVALID INPUT')
+
+
 # Connecting the instrument
 
 rm = pyvisa.ResourceManager()
-
 address = rm.list_resources()
 
 if len(address) == 0:
     print ('No device connected')
     
-else:   
+else:
     print ('VISA address of the connected devices are:')
     
     for i in range(len(address)):
-        print (f"i: {address[i]}")
+        print (f"{i}: {address[i]}")
     
     
-    is_valid_input = False 
+    is_valid_input = False
     
-    while(not is_valid_input):
-        
-        connection_check = input ('Is the VISA address of the SMU listed? (Y/N)')
-        
-        if connection_check == 'Y' or  connection_check == 'y':
+    while not is_valid_input:
+
+        get_address_existence = input ('Is the VISA address of the SMU listed? (Y/N)').lower()
+
+        if get_address_existence == 'y':
             is_valid_input = True
             smu_index = input('Enter the index number of the SMU:')
             k = Keithley2600(address[smu_index])
-            print ('Connected successfully!')
+            is_connected = True
+            print ('Connected successfully!', '/n')
 
-            # Voltage sweep
+            print ('VOLTAGE SWEEP PARAMETERS:', '/n')
+
+            # Voltage sweep parameters
 
             # Source voltage from Channel A
 
             start_volt = float (input ('Set the starting voltage (in Volts) for sweep:'))
 
-            is_valid_input = False
+            target_volt = get_target_volt(start_volt)
+            
+            step_volt = get_step_volt(start_volt, target_volt)
 
-            while (not is_valid_input):
-                target_volt = float (input ('Set the target voltage (in Volts) for sweep:'))
-                if (target_volt > start_volt):
-                    is_valid_input = True
-                else: 
-                    print ('INVALID INPUT: Target voltage must be greater than starting voltage.')
-
-            is_valid_input = False 
-
-            while (not is_valid_input):
-                step_volt = float (input ('Set the size of steps (in Volts) of sweep:')) 
-                if (step_volt <= target_volt - start_volt):
-                   is_valid_input = True
-                else:
-                  print ('INVALID INPUT: Step must be less than or equal to the difference between starting and target voltage')
-
-            if ((target_volt - start_volt) % step_volt == 0):
+            if (target_volt - start_volt) % step_volt == 0:
                 sweep_volt = np.arange (start_volt, target_volt + step_volt, step_volt)
             else:
                 sweep_volt = np.arange (start_volt, target_volt, step_volt)
+            
+            integration_time = get_integration_time()
 
-            is_valid_input = False
+            delay_time = float (input('Set settling delay (in seconds) before each measurement: (NOTE: Setting the value to -1 automatically starts taking measurement as soon as current in stable)'))
 
-            while (not is_valid_input):
-                integ_time = float (input('Set the integration time for each data point (in PLC)'))
-                if (integ_time >= 0.001 and integ_time <= 25):
-                    is_valid_input = True 
-                else: 
-                    print('INVALID INPUT: The integration time must be between 0.001 and 25 PLC.')
+            pulsed = get_sweep_type()
 
-
-                delay_time = float (input('Set settling delay (in seconds) before each measurement: (NOTE: Setting the value to -1 automatically starts taking measuremenet as soon as current in stable)'))
-
-                sweep_type = input ('Choose between pulsed and continuous sweep (enter P or C):')
-
-                is_valid_input = False
-
-            while (not is_valid_input):
-                 if (sweep_type == 'P' or sweep_type == 'p'):
-                    pulsed = True
-                    is_valid_input = True
-                 elif (sweep_type == 'C' or sweep_type == 'c'):
-                    pulsed = False
-                    is_valid_input = True
-                 else:
-                    print ('INVALID INPUT')
-
-            vi_output = k.voltage_sweep_single_smu (k.smua, sweep_volt, integ_time, delay_time, pulsed)
+            # VI measurement
+            vi_output = k.voltage_sweep_single_smu (k.smua, sweep_volt, integration_time, delay_time, pulsed)
 
         
-        elif connection_check == 'N' or connection_check == 'n': 
-            is_valid_input = True 
+        elif get_address_existence == 'n': 
+            is_valid_input = True
             print ('Please ensure that the SMU is connected properly')
-    
+
+
         else: 
-            print ('INVALID INPUT')  
-   
-
-
-
-
-
-
-
-
-
-
-
-
+            print ('INVALID INPUT')
