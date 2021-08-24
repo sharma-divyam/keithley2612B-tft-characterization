966b97976e5ce354d071f2a15e39c6d17ab1526f
sharma-divyam <divyam16september1999@gmail.com>
2021-08-25 03:17:34 +0530

    Resource frame initialize

--- Keithley2612B_voltage_sweep_GUI.py
+++ Keithley2612B_voltage_sweep_GUI.py
@@ -2,8 +2,19 @@ from tkinter import *
 import Keithley2612B_voltage_sweep as kvs
 root = Tk()
 
+# Resource frame
 frame_resc = LabelFrame(root, text = "Resources")
-frame_resc.pack(fill="both", expand="no")
+frame_resc.grid(row = 0, column = 0)
+resc_list_labelname = StringVar()
+resc_list_label = Label (frame_resc, textvariable=resc_list_labelname, anchor= "nw")
+resc_list_labelname.set('Available Resources')
+resc_list_label.grid(row =0, column = 0)
+
+Lb = Listbox (frame_resc)
+Lb.grid(row =  0, column = 1)
+items = ['a','b','c','d','e']
+for i in range(len(items)):
+    Lb.insert (i+1, items[i])
 
 
 root.mainloop()
